eb730914d792d2726835306b0c893a64220b95be
Madeleine Bourdon <m.bourdon@compethance.fr>
2019-09-11 15:59:51 +0200

    portfolio à nouveau scindé en 4 pages au lieu d'une seule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './part/about/about.component';
+import { PortfolioComponent } from './part/portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'portfolio', component: PortfolioComponent},
   { path: 'contact', component: ContactComponent },
 ];
 
