591e2a0e68a117d16fafe557511753c911258d96
Madeleine Bourdon <m.bourdon@compethance.fr>
2020-03-13 17:05:22 +0100

    refonte de la page réalisations, avec une boucle dans un tableau TS

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -9,6 +9,55 @@ import { Title } from '@angular/platform-browser';
 })
 export class PortfolioComponent implements OnInit {
   newTitle = 'Réalisations | Madeleine Bourdon — Portfolio';
+  realisations = [
+    {
+      id: 1,
+      image: '',
+      title: 'Nico nico calendar',
+      description: 
+      [
+        'Site permettant de',
+        'test'
+      ]
+    },
+    {
+      id: 2, 
+      image: '',
+      title: 'Titre 2',
+      description:[
+        'Paragraphe 1',
+        'Paragraphe 2'
+      ]
+    },
+    {
+      id: 3,
+      image: '',
+      title: 'Titre 3',
+      description:[
+        'Paragraphe 1',
+        'Paragraphe 2'
+      ]
+    },
+    {
+      id: 4,
+      image: '',
+      title: 'Titre 4',
+      description:[
+        'Paragraphe 1',
+        'Paragraphe 2'
+      ]
+    },
+    {
+      id: 5,
+      image: '',
+      title: 'Titre 5',
+      description:[
+        'Paragraphe 1',
+        'Paragraphe 2'
+      ]
+    }
+  ];
+
 
   constructor(private titleService: Title) {
     this.titleService.setTitle(this.newTitle);
