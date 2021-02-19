1678eccd9011b79433b8015f7ebb7de7b01af487
Madeleine Bourdon <madeleine.bourdon@gmail.com>
2021-02-19 20:09:54 +0100

    Début de filtres sur la page porfolio

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -2,96 +2,82 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { SharedService } from 'src/app/services/shared.service';
 
-
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {  
-  // realisations = [
-  //   {
-  //     id: 3,
-  //     image: 'compethance.jpg',
-  //     title: 'Compéthance',
-  //     date: 2019,
-  //     interventions: [
-  //       'Découpage'
-  //     ],
-  //     technos: [
-  //       'HTML', 
-  //       'Sass', 
-  //       'JavaScript'
-  //     ],
-  //     description: [
-  //       'Integer varius lectus eu orci rhoncus, eleifend semper turpis iaculis. In mattis ex a nibh bibendum, vel euismod massa condimentum. Ut non nunc vitae velit hendrerit sagittis ut a sapien. Nulla tincidunt sit amet sapien at ullamcorper. Aliquam erat volutpat. Maecenas et rhoncus velit. Proin eget mi at nisi luctus pellentesque. Quisque sit amet metus sed sapien faucibus efficitur sed sed turpis. Duis consequat mi dolor, eget aliquam erat ultrices sit amet. Nullam lacinia eget lacus non placerat. '
-  //     ],
-  //     tags: [
-  //       'pro'
-  //     ]
-  //   },
-  //   {
-  //     id: 2,
-  //     image: 'eloca-ebailleur.jpg',
-  //     title: 'e-loca et e-bailleur',
-  //     date: '2019',
-  //     interventions: [
-  //       'Découpage',
-  //       'Accessibilité'
-  //     ],
-  //     technos: [
-  //       'Ionic',
-  //       'Sass'
-  //     ],
-  //     description: [
-  //       'e-loca et e-bailleur sont 2 éléments d\'un même système de gestion de location Sergic, qui existent à la fois en application pour smartphone ou tablette et en version web.',
-  //       'e-loca permet aux locataires d’accéder au détail de leur compte personnel, à l’ensemble de leurs documents et à toutes les  informations concernant leur location. Elle permet également de communiquer avec son gestionnaire et de signaler un problème ou un sinistre.',
-  //       'J\'ai été chargée de découper les maquettes fournies par Sergic et de les intégrer dans une application Ionic, qui devait comporter plusieurs paramètres d\'accessibilité.'
-  //     ],
-  //     liens: [
-  //       [
-  //         {
-  //           texte: 'Google Play',
-  //           url: 'https://play.google.com/store/apps/details?id=com.sergic.eloca'
-  //         }
-  //       ],
-  //       [
-  //         {
-  //           texte: 'App Store',
-  //           url: 'https://apps.apple.com/fr/app/e-loca/id1488630952'
-  //         }
-  //       ]
-  //     ],
-  //     tags: [
-  //       'pro'
-  //     ]
-  //   },
-  //   {
-  //     id: 1,
-  //     image: '',
-  //     title: 'Legend of Dysmodia',
-  //     date: '2018',
-  //     description: [
-  //       'Paragraphe 1',
-  //       'Paragraphe 2'
-  //     ],
-  //     tags: [
-  //       'ecole'
-  //     ],
-  //     technos: [
-  //       'Symfony',
-  //       'Phaser',
-  //       'Bootstrap'
-  //     ]
-  //   }
-  // ];
+export class PortfolioComponent implements OnInit {
+  completeTagsArray = []
+  filtersCurrentlyChecked = []
+  outputObject:object
 
   constructor(private titleService: Title, public shared: SharedService) {
+    console.log('portfolio')
     // console.log('[porfolio.component.ts] shared.realisations')
-    // console.log(shared.realisations)
-  }
+    console.log(shared.realisations)
 
+    for (var i = 0; i < shared.realisations.length; i++) {
+      console.log('i: ' + i)
+      console.log(shared.realisations[i].interventions)
+
+      if (shared.realisations[i].interventions) {
+        console.log(shared.realisations[i].interventions.length)
+
+        for (let j = 0; j < shared.realisations[i].interventions.length; j++) {
+          if (!this.completeTagsArray.includes(shared.realisations[i].interventions[j])) {
+            this.completeTagsArray.push(shared.realisations[i].interventions[j])
+          }
+          console.log(shared.realisations[i].interventions[j])
+        }
+      }
+    }
+
+    console.log(this.completeTagsArray)
+    this.outputObject = this.shared.realisations
+    
+    this.initializeOutput(this.completeTagsArray)
+  }
+  
+  
   ngOnInit() {
+    if (this.filtersCurrentlyChecked = ['empty']) {
+      this.filtersCurrentlyChecked.splice(0, 1)
+      for (let i=0; i<this.completeTagsArray.length; i++) {
+        this.filtersCurrentlyChecked.push(this.completeTagsArray[i])
+      }
+    }
+    console.log(this.filtersCurrentlyChecked)
   }
 
+  portfolioFiltersChanged(e) {
+    console.log(e)
+    let id = e.target.id.split('-')[1] - 1
+
+    console.log('tagsArray id: ' + id)
+    // console.log(this.tagsArray)
+    // console.log(this.tagsArray[id])
+
+    // console.log(this.outputObject[0])
+
+    // console.log(this.outputObject.filter(obj => { return obj.id === 1}))
+
+    if (e.target.checked === true) {
+      // if it was checked, add the filter
+      console.log('Now checked')
+      // this.filtersCurrentlyChecked.push(this.tagsArray[id])
+    } else {
+      // if it was unchecked, remove it
+      console.log('Now unchecked')
+      // this.filtersCurrentlyChecked.splice(id, 1)
+      // console.log('pouf')
+    }
+
+    console.log(this.filtersCurrentlyChecked)
+    console.log(this.completeTagsArray)
+  }
+
+  initializeOutput(filtersArray) {
+    console.log(filtersArray)
+  }
 }
